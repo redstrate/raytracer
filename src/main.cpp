71480708246af3b7b19234dfb4e90fda897e4229
redstrate <54911369+redstrate@users.noreply.github.com>
2020-05-13 16:53:59 -0400

    Add interactive imgui menus

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <future>
 #include <vector>
 #include <glm/glm.hpp>
+#include <SDL.h>
+#include <array>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
@@ -12,6 +14,10 @@
 #include "image.h"
 #include "lighting.h"
 #include "scene.h"
+#include "glad/glad.h"
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
@@ -25,7 +31,6 @@ const Camera camera = [] {
     
     return camera;
 }();
-constexpr std::string_view model_path = "suzanne.obj";
 constexpr glm::vec3 model_color = glm::vec3(1.0f, 1.0f, 1.0f);
 
 // internal variables
@@ -36,7 +41,7 @@ constexpr int32_t num_tiles_y = height / tile_size;
 
 // globals
 Scene scene = {};
-Image<glm::ivec4, width, height> colors = {};
+Image<glm::vec4, width, height> colors = {};
 
 bool calculate_tile(const int32_t from_x, const int32_t to_width, const int32_t from_y, const int32_t to_height) {
     for (int32_t y = from_y; y < (from_y + to_height); y++) {
@@ -58,7 +63,7 @@ bool calculate_tile(const int32_t from_x, const int32_t to_width, const int32_t 
                 }
                 
                 const glm::vec3 finalColor = model_color * diffuse * (1.0f - shadow);
-                colors.get(x, y) = glm::ivec4(finalColor * 255.0f, 255.0f);
+                colors.get(x, y) = glm::vec4(finalColor, 1.0f);
             }
         }
     }
@@ -66,10 +71,98 @@ bool calculate_tile(const int32_t from_x, const int32_t to_width, const int32_t 
     return true;
 }
 
-int main(int, char*[]) {
-    if(!scene.load_from_file(model_path))
-        return -1;
+GLuint quad_vao = 0;
+GLuint pixel_program = 0;
+GLuint pixels_texture = 0;
+
+void setup_gfx() {
+    // create quad for pixel rendering
+    constexpr std::array vertices = {
+        -0.5f,  0.5f, 0.0f, 0.0f, 0.0f,
+        0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.0f, 1.0f, -1.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, -1.0f // we render it upside down (to opengl) so we flip our tex coord
+    };
     
+    constexpr std::array elements = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    
+    glGenVertexArrays(1, &quad_vao);
+    glBindVertexArray(quad_vao);
+    
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    
+    GLuint ebo = 0;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size() * sizeof(uint32_t), elements.data(), GL_STATIC_DRAW);
+    
+    constexpr std::string_view vertex_glsl =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 in_position;\n"
+        "layout (location = 1) in vec2 in_uv;\n"
+        "out vec2 uv;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(in_position.xyz, 1.0);\n"
+        "   uv = in_uv;\n"
+        "}\n";
+    const char* vertex_src = vertex_glsl.data();
+    
+    constexpr std::string_view fragment_glsl =
+        "#version 330 core\n"
+        "in vec2 uv;\n"
+        "out vec4 out_color;\n"
+        "uniform sampler2D pixel_texture;\n"
+        "void main()\n"
+        "{\n"
+        "    out_color = texture(pixel_texture, uv);\n"
+        "}\n";
+    const char* fragment_src = fragment_glsl.data();
+    
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_src, nullptr);
+    glCompileShader(vertex_shader);
+    
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_src, nullptr);
+    glCompileShader(fragment_shader);
+    
+    pixel_program = glCreateProgram();
+    
+    glAttachShader(pixel_program, vertex_shader);
+    glAttachShader(pixel_program, fragment_shader);
+    glLinkProgram(pixel_program);
+    
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    
+    glGenTextures(1, &pixels_texture);
+    glBindTexture(GL_TEXTURE_2D, pixels_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, nullptr);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void update_texture() {
+    glBindTexture(GL_TEXTURE_2D, pixels_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, colors.array);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void render() {
     std::vector<std::future<bool>> futures;
     for(int32_t y = 0; y < num_tiles_y; y++)
         for(int32_t x = 0; x < num_tiles_x; x++)
@@ -78,8 +171,10 @@ int main(int, char*[]) {
     for(auto& future : futures)
         future.wait();
     
-    std::cout << "rendering done!!" << std::endl;
-    
+    update_texture();
+}
+
+void dump_to_file() {
     uint8_t pixels[width * height * 3] = {};
     int i = 0;
     for (int32_t y = height - 1; y >= 0; y--) {
@@ -90,9 +185,80 @@ int main(int, char*[]) {
             pixels[i++] = c.b;
         }
     }
+
+    stbi_write_png("output.png", width, height, 3, pixels, width * 3);
+}
+
+int main(int, char*[]) {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
     
-    if(stbi_write_png("output.png", width, height, 3, pixels, width * 3) != 1)
-        return -1;
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     
+    SDL_Window* window = SDL_CreateWindow("raytracer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+        
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);
+    
+    gladLoadGL();
+    setup_gfx();
+    
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+    
+    bool running = true;
+    while(running) {
+        SDL_Event event = {};
+        while(SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+
+            if(event.type == SDL_QUIT)
+                running = false;
+        }
+        
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+                
+        ImGui::NewFrame();
+        
+        if(ImGui::BeginMainMenuBar()) {
+            if(ImGui::BeginMenu("File")) {
+                if(ImGui::Button("Load"))
+                    scene.load_from_file("suzanne.obj");
+                
+                ImGui::EndMenu();
+            }
+            
+            ImGui::EndMainMenuBar();
+        }
+        
+        if(ImGui::Button("Render"))
+            render();
+        
+        if(ImGui::Button("Dump to file"))
+            dump_to_file();
+        
+        ImGui::Render();
+        
+        auto& io = ImGui::GetIO();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(pixel_program);
+        glBindVertexArray(quad_vao);
+        glBindTexture(GL_TEXTURE_2D, pixels_texture);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+        SDL_GL_SwapWindow(window);
+    }
+
     return 0;
 }
